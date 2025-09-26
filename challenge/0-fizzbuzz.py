7e1eaff74880bf87bf9c0c76c3acd5cdcc046f93
Dj-kvt <vyckash.william.tresor@gmaiL.com>
2025-09-26 04:33:42 +0200

    add list.h file and correct the file 1and the file 2

--- challenge/0-fizzbuzz.py
+++ challenge/0-fizzbuzz.py
@@ -1,39 +1,39 @@
 #!/usr/bin/python3
-""" FizzBuzz corrigé
+""" FizzBuzz
 """
 import sys
 
 
 def fizzbuzz(n):
     """
-    La fonction FizzBuzz affiche les nombres de 1 à n séparés par un espace.
+    FizzBuzz function prints numbers from 1 to n separated by a space.
 
-    - Pour les multiples de 3, afficher "Fizz".
-    - Pour les multiples de 5, afficher "Buzz".
-    - Pour les multiples de 3 et 5, afficher "FizzBuzz".
+    - For multiples of three print "Fizz" instead of the number and for
+      multiples of five print "Buzz".
+    - For numbers which are multiples of both three and five print "FizzBuzz".
     """
     if n < 1:
         return
 
-    resultat = []
+    tmp_result = []
     for i in range(1, n + 1):
         if (i % 3 == 0) and (i % 5 == 0):
-            resultat.append("FizzBuzz")
+            tmp_result.append("FizzBuzz")
         elif (i % 3 == 0):
-            resultat.append("Fizz")
+            tmp_result.append("Fizz")
         elif (i % 5 == 0):
-            resultat.append("Buzz")
+            tmp_result.append("Buzz")
         else:
-            resultat.append(str(i))
-    print(" ".join(resultat))
+            tmp_result.append(str(i))
+    print(" ".join(tmp_result))
 
 
 if __name__ == '__main__':
     if len(sys.argv) <= 1:
-        print("Nombre manquant")
-        print("Utilisation: ./0-fizzbuzz.py <nombre>")
-        print("Exemple: ./0-fizzbuzz.py 89")
+        print("Missing number")
+        print("Usage: ./0-fizzbuzz.py <number>")
+        print("Example: ./0-fizzbuzz.py 89")
         sys.exit(1)
 
-    nombre = int(sys.argv[1])
-    fizzbuzz(nombre)
+    number = int(sys.argv[1])
+    fizzbuzz(number)
